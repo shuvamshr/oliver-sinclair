04b25bb87cd0aecfa615fc5d152d7a72d6a17764
shuvamshr <shuvamshrestha408@gmail.com>
2023-11-21 21:32:21 +1100

    update: responsive header

--- src/global/Header/index.jsx
+++ src/global/Header/index.jsx
@@ -1,11 +1,27 @@
 import CompanyLogo from "./assets/oliver-sinclair-logo.svg";
+import MenuIcon from "./assets/menu.svg";
+
+import Typography from "../../components/Typography";
+import Button from "../../components/Button";
 
 export default function Index() {
   return (
     <>
       <div className="container-fluid bg-white border-gray-200 border-b-[0.5px] fixed top-0 left-0 right-0 ">
-        <div className="container mx-auto h-[84px] flex flex-row items-center lg:px-0 px-6">
+        <div className="container mx-auto h-[84px]  items-center lg:px-0 px-6 flex flex-row justify-between">
           <img src={CompanyLogo} className="lg:w-[255px] w-[220px]" alt="" />
+          <div className="lg:flex lg-flex-row lg:gap-10  hidden">
+            <Typography type="head-fix-base-semibold">home</Typography>
+            <Typography type="body-fix-base-regular">services</Typography>
+            <Typography type="body-fix-base-regular">case studies</Typography>
+            <Typography type="body-fix-base-regular">blogs</Typography>
+            <Typography type="body-fix-base-regular">contact</Typography>
+          </div>
+          <div className="lg:flex lg:flex-row lg:gap-3 hidden">
+            <Button type="secondary-fix-medium-fit">View Bundles</Button>
+            <Button type="primary-fix-medium-fit">Who's Oliver?</Button>
+          </div>
+          <img src={MenuIcon} className="lg:hidden" alt="" />
         </div>
       </div>
     </>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ export default function () {
   return (
     <>
       <Header />
-      <div className="container-fluid bg-red-100 lg:mt-12 mt-6">
+      <div className="container-fluid bg-red-100 mt-6">
         <div className="container mx-auto lg:px-0 px-6 py-32">
           <div className="grid grid-cols-12">
             <div className="lg:col-span-6 lg:gap-10 col-span-12 gap-6 flex flex-col justify-center">
