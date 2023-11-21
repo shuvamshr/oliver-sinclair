429752e7e4954d669117d575cf4d49e5fc72e898
shuvamshr <shuvamshrestha408@gmail.com>
2023-11-21 17:06:17 +1100

    update: fix responsive logo

--- src/global/Header/index.jsx
+++ src/global/Header/index.jsx
@@ -5,7 +5,7 @@ export default function Index() {
     <>
       <div className="container-fluid bg-white border-gray-200 border-b-[0.5px] fixed top-0 left-0 right-0 ">
         <div className="container mx-auto h-[84px] flex flex-row items-center lg:px-0 px-6">
-          <img src={CompanyLogo} alt="" />
+          <img src={CompanyLogo} className="lg:w-[255px] w-[220px]" alt="" />
         </div>
       </div>
     </>
