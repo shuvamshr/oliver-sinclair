7676b5f3ef6e2536a1e1f2ae487380c578178756
shuvamshr <shuvamshrestha408@gmail.com>
2023-11-21 15:10:14 +1100

    add: fluid container

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,39 +4,46 @@ import Typography from "../../components/Typography";
 export default function () {
   return (
     <>
-      <div className="container mx-auto lg:max-2xl:px-0 px-6 py-32 bg-red-100">
-        <div className="grid grid-cols-12">
-          <div className="lg:max-2xl:col-span-6 lg:max-2xl:gap-10 col-span-12 gap-6 flex flex-col ">
-            <div className="flex flex-col gap-2">
-              <Typography type="head-res-lg-medium">
-                NEW UX BUNDLE ADDED
-              </Typography>
-              <Typography type="head-res-5xl-semibold">
-                Crafting Creativity <br /> to Captivate.
-                <span className="text-red-900 font-bold italic"> No Cap.</span>
-              </Typography>
-            </div>
-            <Typography type="body-res-xl-regular">
-              Embark on a digital journey where captivation knows no bounds. At
-              Oliver Sinclair, we're devoted to crafting experiences that
-              transcend expectations.
-            </Typography>
-
-            <div className="flex flex-col gap-10">
-              <div className="flex flex-col lg:max-2xl:flex-row lg:max-2xl:gap-6 gap-4">
-                <Button type="primary-res-large-block">Who's My Oliver?</Button>
-                <Button type="secondary-res-large-block">
-                  View Service Bundles
-                </Button>
+      <div className="container-fluid bg-red-100">
+        <div className="container mx-auto lg:max-2xl:px-0 px-6 py-32">
+          <div className="grid grid-cols-12">
+            <div className="lg:max-2xl:col-span-6 lg:max-2xl:gap-10 col-span-12 gap-6 flex flex-col ">
+              <div className="flex flex-col gap-2">
+                <Typography type="head-res-lg-medium">
+                  NEW UX BUNDLE ADDED
+                </Typography>
+                <Typography type="head-res-5xl-semibold">
+                  Crafting Creativity <br /> to Captivate.
+                  <span className="text-red-900 font-bold italic">
+                    {" "}
+                    No Cap.
+                  </span>
+                </Typography>
               </div>
-              <Typography type="body-fix-sm-regular">
-                learn more about our services, or figure out who oliver is
+              <Typography type="body-res-xl-regular">
+                Embark on a digital journey where captivation knows no bounds.
+                At Oliver Sinclair, we're devoted to crafting experiences that
+                transcend expectations.
               </Typography>
-            </div>
-          </div>
 
-          <div className="lg:max-2xl:col-start-8 lg:max-2xl:col-span-5 col-start-1 col-span-12 flex bg-gray-100 h-full rounded-tl-[5rem] rounded-br-[5rem]">
-            <div></div>
+              <div className="flex flex-col gap-10">
+                <div className="flex flex-col lg:max-2xl:flex-row lg:max-2xl:gap-6 gap-4">
+                  <Button type="primary-res-large-block">
+                    Who's My Oliver?
+                  </Button>
+                  <Button type="secondary-res-large-block">
+                    View Service Bundles
+                  </Button>
+                </div>
+                <Typography type="body-fix-sm-regular">
+                  learn more about our services, or figure out who oliver is
+                </Typography>
+              </div>
+            </div>
+
+            <div className="lg:max-2xl:col-start-8 lg:max-2xl:col-span-5 col-start-1 col-span-12 flex bg-gray-100 h-full rounded-tl-[5rem] rounded-br-[5rem]">
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
