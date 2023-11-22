90cf5c985ccbc3d98228192b69dcddbae9cd132f
shuvamshr <shuvamshrestha408@gmail.com>
2023-11-23 10:55:48 +1100

    update: implement new component on sections

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import Header from "../../global/Header";
 
 import Button from "../../components/Button";
-
 import Typography from "../../components/Typography";
+import Container from "../../components/Container";
+import Row from "../../components/Row";
+import Col from "../../components/Col";
 
 import CreativityImg from "./assets/creativity.png";
 import GroupworkImg from "./assets/groupwork.png";
@@ -18,160 +20,144 @@ export default function () {
   return (
     <>
       <Header />
-      <div className="container-fluid bg-red-100 lg:mt-6 mt-24">
-        <div className="container mx-auto lg:px-0 px-6 lg:py-32 py-16">
-          <div className="grid grid-cols-12">
-            <div className="lg:col-span-6 lg:gap-10 col-span-12 gap-6 flex flex-col justify-center">
-              <div className="flex flex-col gap-2">
-                <Typography size="res-lg" weight="medium">
-                  NEW UX BUNDLE ADDED
-                </Typography>
 
-                <Typography size="res-5xl" weight="semibold">
-                  Crafting Creativity <br /> to Captivate.
-                  <span className="text-red-900 font-bold italic">
-                    {" "}
-                    No Cap.
-                  </span>
-                </Typography>
-              </div>
-
-              <Typography type="serif" size="res-xl">
-                Embark on a digital journey where captivation knows no bounds.
-                At Oliver Sinclair, we're devoted to crafting experiences that
-                transcend expectations.
+      <Container>
+        <Row>
+          <Col span="6">
+            <div className="flex flex-col gap-2">
+              <Typography size="res-lg" weight="medium">
+                NEW UX BUNDLE ADDED
               </Typography>
 
-              <div className="flex flex-col gap-10">
-                <div className="flex flex-col lg:flex-row lg:gap-6 gap-4">
-                  <Button type="primary-res-large-block">
-                    Who's My Oliver?
-                  </Button>
-                  <Button type="secondary-res-large-block">
-                    View Service Bundles
-                  </Button>
-                </div>
-                <Typography type="serif" size="sm">
-                  learn more about our services, or figure out who oliver is
-                </Typography>
-              </div>
-            </div>
-
-            <div className="lg:col-start-8 lg:col-span-5 col-span-12 justify-center">
-              <img src={CreativityImg} className="lg:pt-0 pt-10" alt="" />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="container-fluid bg-gray-900">
-        <div className="container mx-auto lg:px-0 px-6 lg:py-32 py-16">
-          <div className="grid grid-cols-12">
-            <div className="lg:col-span-10 lg:col-start-2 lg:gap-12 col-span-12 gap-12 flex flex-col justify-center">
-              <div className="flex flex-col gap-6">
-                <Typography
-                  size="res-4xl"
-                  weight="semibold"
-                  color="deepWhite"
-                  align="res-center"
-                >
-                  Certified expertise from over{" "}
-                  <span className="text-red-900 font-bold italic">10+</span>{" "}
-                  industries,
-                  <br className="lg:block hidden" /> universities and events
-                </Typography>
-                <Typography
-                  type="serif"
-                  size="res-lg"
-                  color="liteWhite"
-                  align="res-center"
-                >
-                  our team of dedicated professionals comprises individuals with
-                  a diverse array of skills and knowledge acquired through
-                  certified expertise from various industries, renowned
-                  universities, and impactful events
-                </Typography>
-              </div>
-              <div className="flex lg:flex-wrap justify-between gap-14 items-center overflow-x-auto snap-x">
-                <img src={UTSLogo} alt="" className="snap-center" />
-                <img src={AppleLogo} alt="" className="snap-center" />
-                <img src={DolbyLogo} alt="" className="snap-center" />
-                <img src={LeedsLogo} alt="" className="snap-center" />
-                <img src={OliverWhiteLogo} alt="" className="snap-center" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="container-fluid bg-red-100">
-        <div className="container mx-auto lg:px-0 px-6 lg:py-32 py-16">
-          <div className="grid grid-cols-12">
-            <div className="lg:col-span-5 col-span-12 justify-center">
-              <img src={GroupworkImg} className="lg:pb-0 pb-10" alt="" />
-            </div>
-            <div className="lg:col-start-7 lg:col-span-6 lg:gap-10 col-span-12 gap-6 flex flex-col justify-center">
-              <Typography size="res-4xl" weight="semibold">
-                Whom exactly is..{" "}
-                <span className="text-red-900 font-bold italic">Oliver?</span>
+              <Typography size="res-5xl" weight="semibold">
+                Crafting Creativity <br /> to Captivate.
+                <span className="text-red-900 font-bold italic"> No Cap.</span>
               </Typography>
-              <Typography type="serif" size="res-lg">
-                we inspire businesses to conceive and craft the digital realms
-                of the future. Success is a collective voyage with oliver, and
-                you’re just one click away from getting your way. our team of
-                dedicated professionals comprises individuals with a diverse
-                array of skills. <br />
-                <br />
-                knowledge acquired through certified expertise from various
-                industries, renowned universities, and impactful events
+            </div>
+            <Typography type="serif" size="res-xl">
+              Embark on a digital journey where captivation knows no bounds. At
+              Oliver Sinclair, we're devoted to crafting experiences that
+              transcend expectations.
+            </Typography>
+            <div className="flex flex-col gap-10">
+              <div className="flex flex-col lg:flex-row lg:gap-6 gap-4">
+                <Button type="primary-res-large-block">Who's My Oliver?</Button>
+                <Button type="secondary-res-large-block">
+                  View Service Bundles
+                </Button>
+              </div>
+              <Typography type="serif" size="sm">
+                learn more about our services, or figure out who oliver is
               </Typography>
+            </div>
+          </Col>
+          <Col span="5" start="8">
+            <img src={CreativityImg} alt="" />
+          </Col>
+        </Row>
+      </Container>
 
-              <div className="flex flex-col gap-10">
-                <div className="flex flex-col lg:flex-row lg:gap-6 gap-4">
-                  <Button type="primary-res-large-block">
-                    Who's My Oliver?
-                  </Button>
-                  <Button type="secondary-res-large-block">
-                    View Service Bundles
-                  </Button>
-                </div>
-              </div>
+      <Container color="offBlack">
+        <Row>
+          <Col span="10" start="2" justify="res-center">
+            <div className="flex flex-col gap-6">
+              <Typography
+                size="res-4xl"
+                weight="semibold"
+                color="deepWhite"
+                align="res-center"
+              >
+                Certified expertise from over{" "}
+                <span className="text-red-900 font-bold italic">10+</span>{" "}
+                industries,
+                <br className="lg:block hidden" /> universities and events
+              </Typography>
+              <Typography
+                type="serif"
+                size="res-lg"
+                color="liteWhite"
+                align="res-center"
+              >
+                our team of dedicated professionals comprises individuals with a
+                diverse array of skills and knowledge acquired through certified
+                expertise from various industries, renowned universities, and
+                impactful events
+              </Typography>
             </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+          <Col span="10" start="2">
+            <div className="flex lg:flex-wrap justify-between gap-14 items-center overflow-x-auto snap-x">
+              <img src={UTSLogo} alt="" className="snap-center" />
+              <img src={AppleLogo} alt="" className="snap-center" />
+              <img src={DolbyLogo} alt="" className="snap-center" />
+              <img src={LeedsLogo} alt="" className="snap-center" />
+              <img src={OliverWhiteLogo} alt="" className="snap-center" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
-      <div className="container-fluid bg-white">
-        <div className="container mx-auto lg:px-0 px-6 lg:py-32 py-16">
-          <div className="grid grid-cols-12 lg:gap-12 gap-6">
-            <div className="lg:col-span-8 lg:col-start-3 col-span-12">
-              <img src={ProcessImg} className="lg:pb-0 pb-10" alt="" />
-            </div>
-            <div className="lg:col-span-10 lg:col-start-2 lg:gap-12 col-span-12 gap-12 flex flex-col justify-center">
-              <div className="flex flex-col gap-6">
-                <Typography size="res-4xl" weight="semibold" align="res-center">
-                  All your design needs bundled.{" "}
-                  <span className="text-red-900 font-bold italic">
-                    Take your pick.
-                  </span>
-                </Typography>
-                <Typography type="serif" size="res-lg" align="res-center">
-                  our team of dedicated professionals comprises individuals with
-                  a diverse array of skills and knowledge acquired through
-                  certified expertise from various industries, renowned
-                  universities, and impactful events
-                </Typography>
-              </div>
-            </div>
-            <div className="flex flex-col  lg:flex-row lg:gap-6 gap-4 lg:col-span-8 lg:col-start-3 col-span-12">
+      <Container>
+        <Row>
+          <Col span="5">
+            <img src={GroupworkImg} alt="" />
+          </Col>
+          <Col span="6" start="7">
+            <Typography size="res-4xl" weight="semibold">
+              Whom exactly is..{" "}
+              <span className="text-red-900 font-bold italic">Oliver?</span>
+            </Typography>
+            <Typography type="serif" size="res-lg">
+              we inspire businesses to conceive and craft the digital realms of
+              the future. Success is a collective voyage with oliver, and you’re
+              just one click away from getting your way. our team of dedicated
+              professionals comprises individuals with a diverse array of
+              skills. <br />
+              <br />
+              knowledge acquired through certified expertise from various
+              industries, renowned universities, and impactful events
+            </Typography>
+            <div className="flex flex-col lg:flex-row lg:gap-6 gap-4">
               <Button type="primary-res-large-block">Who's My Oliver?</Button>
               <Button type="secondary-res-large-block">
                 View Service Bundles
               </Button>
             </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container color="white">
+        <Row>
+          <Col span="8" start="3" justify="res-center">
+            <img src={ProcessImg} alt="" />
+          </Col>
+          <Col span="10" start="2" justify="res-center">
+            <div className="flex flex-col gap-6">
+              <Typography size="res-4xl" weight="semibold" align="res-center">
+                All your design needs bundled.{" "}
+                <span className="text-red-900 font-bold italic">
+                  Take your pick.
+                </span>
+              </Typography>
+              <Typography type="serif" size="res-lg" align="res-center">
+                our team of dedicated professionals comprises individuals with a
+                diverse array of skills and knowledge acquired through certified
+                expertise from various industries, renowned universities, and
+                impactful events
+              </Typography>
+            </div>
+          </Col>
+          <Col span="8" start="3" justify="res-center">
+            <div className="flex flex-col lg:flex-row lg:gap-6 gap-4">
+              <Button type="primary-res-large-block">Who's My Oliver?</Button>
+              <Button type="secondary-res-large-block">
+                View Service Bundles
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
