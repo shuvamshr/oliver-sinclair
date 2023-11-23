6f45bd8154b4369e73395a613b0f95387755ac58
shuvamshr <shuvamshrestha408@gmail.com>
2023-11-23 14:53:21 +1100

    add: alignment properties

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,11 +1,13 @@
 const rowStyle = {
   medium: "gap-12",
   small: "gap-6",
+
+  center: "flex flex-col justify-center",
 };
 
 export default function Row(props) {
-  const { gap = "medium" } = props;
-  const rowType = `grid grid-cols-6 lg:grid-cols-12 ${rowStyle[gap]}`;
+  const { gap = "medium", align } = props;
+  const rowType = `grid grid-cols-6 lg:grid-cols-12 ${rowStyle[gap]} ${rowStyle[align]}`;
   return (
     <>
       <div className={rowType}>{props.children}</div>
