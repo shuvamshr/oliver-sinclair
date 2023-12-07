f9423635ac9af8dab7d3c81cc3dc7956df47f925
shuvamshr <shuvamshrestha408@gmail.com>
2023-12-07 13:40:43 +1100

    add: image hostname

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "images.ctfassets.net",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "downloads.ctfassets.net",
+        port: "",
+      },
     ],
   },
 };
