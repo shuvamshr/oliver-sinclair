8d96b318d80b7da980c085603e579e2ac4a0f3b3
shuvamshr <shuvamshrestha408@gmail.com>
2023-12-11 17:37:48 +1100

    add: video and image embed functionality

--- src/app/components/contentful/richtext/index.tsx
+++ src/app/components/contentful/richtext/index.tsx
@@ -1,5 +1,40 @@
+import { BLOCKS, INLINES, MARKS } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
 
-export default function RichText(props: any) {
-  return <>{documentToReactComponents(props.content)}</>;
+const options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ENTRY]: (node: any) => {
+      return (
+        <iframe
+          className="rounded-lg m-0 aspect-video"
+          height="400"
+          width="100%"
+          src={node.data.target.fields.embedUrl}
+          title={node.data.target.fields.title}
+          allowFullScreen={true}
+        />
+      );
+    },
+    [BLOCKS.EMBEDDED_ASSET]: (node: any) => {
+      return (
+        <div className="aspect-video relative">
+          <Image
+            src={"https:" + node.data.target.fields.file.url}
+            className="rounded-lg m-0"
+            alt="Card Thumbnail"
+            fill={true}
+          />
+        </div>
+      );
+    },
+  },
+};
+
+interface RichTextProps {
+  content?: any;
+}
+
+export default function RichText({ content }: RichTextProps) {
+  return <>{documentToReactComponents(content, options)}</>;
 }
