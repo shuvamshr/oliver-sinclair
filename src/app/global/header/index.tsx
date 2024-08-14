4eb323c78c9f9b3733d7b6420fe120ed2bd8b056
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-08-14 13:46:17 +1000

    update linkedin link

--- src/app/global/header/index.tsx
+++ src/app/global/header/index.tsx
@@ -194,7 +194,7 @@ export default function Header() {
               title="Get in Touch"
               type="secondary"
               size="medium"
-              link="https://www.linkedin.com/in/shuvam-s-976186184/"
+              link="https://www.linkedin.com/in/shuvamshr/"
               newTab={true}
             />
             <Button
