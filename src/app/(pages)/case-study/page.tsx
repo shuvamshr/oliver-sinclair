8e189ab8b3c2464201b8ad9d6f89a0bedd558330
Shuvam Shrestha <shuvamshrestha408@gmail.com>
2024-10-02 16:51:14 +1000

    update: content

--- src/app/(pages)/case-study/page.tsx
+++ src/app/(pages)/case-study/page.tsx
@@ -13,30 +13,27 @@ export default async function CaseStudies() {
             <div className="col-span-12 lg:col-span-10 lg:col-start-2 flex flex-col gap-10">
               <div className="flex flex-col gap-4">
                 <h1 className="font-sans font-semibold text-2xl lg:text-3xl lg:text-center text-gray-800 lg:leading-normal leading-relaxed">
-                  An insight into our{" "}
-                  <span className="text-red-900 font-bold italic">
-                    Creativity.
-                  </span>
+                  Digital experiences for{" "}
+                  <span className="text-red-900 font-bold italic">People.</span>
                 </h1>
                 <p className="font-serif font-regular text-base lg:text-center text-gray-700 lg:leading-normal leading-relaxed">
-                  Witness the power of problem-solving in action – delve into
-                  our Projects Gallery, where challenges transform into success
-                  stories, showcasing innovative solutions that drive results
+                  Pproblem-solving in action – delve into the latest Projects
+                  Gallery, where challenges transform into success stories.
                 </p>
               </div>
             </div>
             <div className="col-span-12 lg:col-span-8 lg:col-start-3 flex flex-col lg:flex-row gap-3 lg:gap-5">
               <Button
-                title="Get Started With Your Project"
+                title="Get in Touch"
                 display="block"
                 link="https://www.linkedin.com/in/shuvamshr/"
                 newTab={true}
               />
               <Button
-                title="View Service Bundles"
+                title="View Blogs"
                 type="secondary"
                 display="block"
-                link="services"
+                link="blogs"
               />
             </div>
           </div>
